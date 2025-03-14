0912bd6240a0a95b5625d95c88884868b3e0f876
quyc <quycsff@gmail.com>
2025-03-14 16:48:20 +0000

    springboot

---
+++ src/main/java/com/quyc/devcontainer/DevContainerAppApplication.java
@@ -0,0 +1,13 @@
+package com.quyc.devcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevContainerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevContainerAppApplication.class, args);
+	}
+
+}
